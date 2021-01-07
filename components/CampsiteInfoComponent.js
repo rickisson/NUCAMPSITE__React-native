c31f0f0220404e27b3d62e60d23b19f27f843d7c
Eric .O <ericosuala@gmail.con>
2021-01-07 16:42:07 -0600

    Redux Adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,12 +3,18 @@ import { Card,Icon } from 'react-native-elements';
 import { Text, View, ScrollView, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return{
         campsites:state.campsites,
-        comments:state.comments
+        comments:state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 
@@ -65,16 +71,10 @@ function RenderCampsite(props) {
 
 class CampsiteInfo extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-            favorite: false
-        };
-    }
+    
 
-    markFavorite() {
-        this.setState({favorite: true});
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
     }
    
     //This helps to set the naviagion header//
@@ -89,8 +89,8 @@ class CampsiteInfo extends Component {
         return (
             <ScrollView>
                 <RenderCampsite campsite={campsite}
-                     favorite={this.state.favorite}
-                     markFavorite={() => this.markFavorite()}
+                    favorite={this.props.favorites.includes(campsiteId)}
+                     markFavorite={() => this.markFavorite(campsiteId)}
                  />
                      
                 <RenderComments comments={comments} />
