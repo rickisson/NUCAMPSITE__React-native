0bc9bf84167bfd64e68425ab6708ce80e378d5e6
Eric .O <ericosuala@gmail.con>
2021-01-11 11:55:50 -0600

    Swipe and Delete Favortes

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -154,6 +154,12 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
+
+
 export const postComment = (campsiteId,rating,author,text) => dispatch => {
 
     const newComment = {
