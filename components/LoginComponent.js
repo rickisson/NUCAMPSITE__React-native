5ec8907b7e35937474376c3528bb98b834d3afc7
Eric .O <ericosuala@gmail.con>
2021-01-23 12:36:44 -0600

    accessing react native elements

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,8 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 
 
 
@@ -159,9 +161,42 @@ class RegisterTab extends Component{
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri)
+                //this.setState({imageUrl: capturedImage.uri});//
             }
         }
+    }
+    
+    //processing an image//
+    processImage = async (imgUri) => {
+            const processedImage = await ImageManipulator.manipulateAsync(
+                imgUri,
+                 [{resize:{width:400}}],
+                {compress: 1, format:ImageManipulator.SaveFormat.PNG}
+                 
+            );
+            console.log(processedImage);
+            this.setState({imageUrl:processedImage.uri});
+    }
+    
+    
+    getImageFromGallery = async () => {
+        const  cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+       
+        if (cameraPermission.status === 'granted' && cameraRollPermissions.status === 'granted') {
+          const  capturedImage = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [1, 1],
+            
+          });
+          if (!capturedImage.cancelled) {
+            console.log(capturedImage);
+            this.processImage(capturedImage.uri)
+          }
+       }
+
     }
 
     handleRegister() {
@@ -191,6 +226,12 @@ class RegisterTab extends Component{
                         title='Camera'
                         onPress={this.getImageFromCamera}
                     />
+                     <Button
+                       title="Gallery"
+                       onPress={this.getImageFromGallery}
+                      
+                    />
+
                 </View>
                 <Input
                     placeholder='Username'
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,7 +4,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image , Alert, ToastAndroid} from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
@@ -16,6 +16,7 @@ import { fetchCampsites, fetchComments, fetchPromotions,
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
 import Login from './LoginComponent';
+import NetInfo from '@react-native-community/netinfo';
 
     
     const mapDispatchToProps = {
@@ -335,7 +336,54 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
+
+        NetInfo.fetch().then(connectionInfo => {
+            this.showNetInfo(connectionInfo)
+            // (Platform.OS === 'ios')
+            //     ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+            //     : ToastAndroid.show('Initial Network Connectivity Type: ' +
+            //         connectionInfo.type, ToastAndroid.LONG);
+        });
+
+        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+            this.handleConnectivityChange(connectionInfo);
+        });
+    
     }
+
+    showNetInfo = async () => {
+        const connectionInfo = await
+        (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeNetInfo();
+    }
+
+    handleConnectivityChange = connectionInfo => {
+        let connectionMsg = 'You are now connected to an active network.';
+        switch (connectionInfo.type) {
+            case 'none':
+                connectionMsg = 'No network connection is active.';
+                break;
+            case 'unknown':
+                connectionMsg = 'The network connection state is now unknown.';
+                break;
+            case 'cellular':
+                connectionMsg = 'You are now connected to a cellular network.';
+                break;
+            case 'wifi':
+                connectionMsg = 'You are now connected to a WiFi network.';
+                break;
+        }
+        (Platform.OS === 'ios')
+            ? Alert.alert('Connection change:', connectionMsg)
+            : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+    }
+
     
     render() {
         return (
